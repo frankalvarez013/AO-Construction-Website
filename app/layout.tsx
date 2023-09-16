52a16eb1328be035f23ccd045cd95f1866df319d
Frank Alvarez <frankalvarez013@gmail.com>
2023-09-15 23:49:07 -0700

    BoilterPlate & Scaffolding
    Added empty .tsx files that consisted of components to create the layout of the Home webpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,117 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          {/* <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+              <svg
+                className="fill-current h-8 w-8 mr-2"
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+              </svg>
+              <span className="font-semibold text-xl tracking-tight">
+                Tailwind CSS
+              </span>
+            </div>
+            <div className="block lg:hidden">
+              <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+              <div className="text-sm lg:flex-grow">
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                >
+                  Docs
+                </a>
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                >
+                  Examples
+                </a>
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+                >
+                  Blog
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+          </nav> */}
+          <nav>
+            <ul className="flex justify-between content-center m-5">
+              <li className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  {' '}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+                  />
+                </svg>
+                <h4>AO Construction</h4>
+              </li>
+              <div className="flex mr-6">
+                <li>Home</li>
+                <li>About us</li>
+                <li>Locations</li>
+                <li>{`FAQ's`}</li>
+                <li>
+                  <button>
+                    <h4>Contact Us</h4>
+                  </button>
+                </li>
+              </div>
+              {/* <li>SYMBOL</li> */}
+            </ul>
+          </nav>
+        </header>
+        {children}
+        <footer className="py-10">
+          <ul>
+            <li>
+              <ul className="flex">
+                <li className="mr-6">FSYMBOL</li>
+                <li className="mr-6">TSYMBOL</li>
+                <li>LSYMBOL</li>
+              </ul>
+            </li>
+            <li>LOCATION</li>
+            <li>PHONE#</li>
+            <li>EMAIL</li>
+          </ul>
+        </footer>
+      </body>
     </html>
   )
 }
